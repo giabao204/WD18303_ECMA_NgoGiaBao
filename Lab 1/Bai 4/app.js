4c8034a1df5275c646ab997f83057e3e8d33d6de
baongpc05497 <145195574+giabao204@users.noreply.github.com>
2024-01-06 18:48:39 +0700

    Lab 1

--- Lab 1/Bai 4/app.js
+++ Lab 1/Bai 4/app.js
@@ -4,7 +4,7 @@ fetch("https://datausa.io/api/data?drilldowns=Nation&measures=Population")
             console.log(data);
             let array = data.data;
             let html = document.getElementById('table');
-            let index =1;
+            let cow =1;
             let child_html = `<table class="table table-success table-striped">
                     <thead>
                     <tr>
@@ -20,7 +20,7 @@ fetch("https://datausa.io/api/data?drilldowns=Nation&measures=Population")
                 console.log(element);
                 child_html += `<tbody>
                         <tr>
-                            <td>${index++}</td>
+                            <td>${cow++}</td>
                             <td>${element.Nation}</td>
                             <td>${element.Year}</td>
                             <td>${element.Population}</td>
@@ -45,7 +45,7 @@ fetch("https://65929f4fbb129707198fe18e.mockapi.io/tinhpv10/students")
 
         let html = document.getElementById('tables');
 
-        let index = 1;
+        let cow = 1;
 
         let child_html = `<table class="table table-borderless">
     <thead>
@@ -61,7 +61,7 @@ fetch("https://65929f4fbb129707198fe18e.mockapi.io/tinhpv10/students")
             child_html += `
       <tbody>
         <tr>
-          <td>${index++}</td>
+          <td>${cow++}</td>
           <td><img src="${element.avatar}" alt="${element.name} class="style="width: 60px;""</td>
           <td>${element.name}</td>
           <td>${element.createdAt}</td>
